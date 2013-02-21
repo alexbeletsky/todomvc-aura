d6e31393ffd3a010a0add0182f0f27f428f93a6f
alexander.beletsky <alexander.beletsky@gmail.com>
2013-02-21 17:12:35 +0200

    create new task added

--- src/widgets/tasks/main.js
+++ src/widgets/tasks/main.js
@@ -1,4 +1,4 @@
-define(function () {
+define(['underscore'], function (_) {
 
     return {
         template: 
@@ -8,7 +8,19 @@ define(function () {
                 <ul id="todo-list"></ul>\
             </section>',
 
+        taskTemplate: 
+            '<li>\
+                <div class="view">\
+                    <input class="toggle" type="checkbox">\
+                    <label><%= description %></label>\
+                    <button class="destroy"></button>\
+                </div>\
+                <input class="edit" value="<%= description %>">\
+            </li>',
+
         initialize: function () {
+            _.bindAll(this);
+
             this.render();
             this.cacheElements();
             this.attachEvents();
@@ -21,13 +33,15 @@ define(function () {
         cacheElements: function () {
             this.$todoList = this.$el.find('#todo-list');
             this.$toggleAll = this.$el.find('#toggle-all');
-
         },
 
         attachEvents: function () {
-            this.sandbox.on('tasks/createNewTask', function () {
-                alert('aaaa');
-            });
+            this.sandbox.on('tasks/createNewTask', this.createNewTask);
+        },
+
+        createNewTask: function(task) {
+            var taskHtml = _.template(this.taskTemplate, task);
+            this.$todoList.append(taskHtml);
         }
     };
 
